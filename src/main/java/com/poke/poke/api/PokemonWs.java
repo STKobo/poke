df369ddd597a86515e08f80ca98854d791fe4d7e
Stephane_tcha <stephanetcha@gmail.com>
2023-03-02 19:10:22 +0100

    impl

--- src/main/java/com/poke/poke/api/PokemonWs.java
+++ src/main/java/com/poke/poke/api/PokemonWs.java
@@ -2,6 +2,7 @@ package com.poke.poke.api;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,29 +13,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.poke.poke.pojo.Pokemon;
+import com.poke.poke.service.PokemonService;
 
 @RestController
 @RequestMapping(ApiRegistration.REST_PREFIX + ApiRegistration.REST_POKEMON)
 public class PokemonWs {
 
+    @Autowired
+    private PokemonService service;
+
     @GetMapping
     public List<Pokemon> getAllPokemon(){
-        return null;
+        return service.getAllPokemon();
     }
 
-    @PostMapping
+    @GetMapping("{name}")
+    public List<Pokemon> getAllPokemonByName(@PathVariable(name = "name")){
+        return service.getAllPokemon(nom);
+    }
+
+    @PostMapping 
     public void createPokemon(@RequestBody Pokemon pokemon){
-        
+        service.createPokemon(pokemon);
     }
 
     @PutMapping("{name}")
-    public void updatePokemon(@PathVariable(name = "name") String nom){
-
+    public void updatePokemon(@PathVariable(name = "name") String nom, @RequestBody Pokemon pokemon){
+        service.updatePokemon(nom, pokemon);
     }
 
     @DeleteMapping("{name}")
     public void deletePokemon(@PathVariable(name = "name") String nom){
-
+        service.deletePokemon(nom);
     }
 
 }
--- src/main/java/com/poke/poke/service/impl/PokemonServiceImpl.java
+++ src/main/java/com/poke/poke/service/impl/PokemonServiceImpl.java
@@ -1,5 +1,46 @@
 package com.poke.poke.service.impl;
 
-public class PokemonServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.poke.poke.pojo.Pokemon;
+import com.poke.poke.service.PokemonService;
+
+@Service
+public class PokemonServiceImpl implements PokemonService{
+
+    private static List<Pokemon> maListe = new ArrayList<>();
+
+    @Override
+    public List<Pokemon> getAllPokemon() {
+        return maListe;
+    }
+
+    @Override
+    public List<Pokemon> getAllPokemonByName(String name) {
+        return maListe.stream().filter(pokemon -> pokemon.getSurname().equals(name)).collect(Collectors.toList());
+    }
+
+    @Override
+    public void updatePokemon(String pokemonName, Pokemon pokemon) {
+        this.deletePokemon(pokemonName);
+        maListe.add(pokemon);
+    }
+
+    @Override
+    public void createPokemon(Pokemon pokemon) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createPokemon'");
+    }
+
+    @Override
+    public void deletePokemon(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deletePokemon'");
+    }
+    
     
 }
