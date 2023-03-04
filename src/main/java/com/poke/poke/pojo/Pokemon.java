95d61108341889ad818e7b8dfa4a65cdaf1f28b3
Stephane_tcha <stephanetcha@gmail.com>
2023-03-04 23:55:23 +0100

    test jpa

--- src/main/java/com/poke/poke/pojo/Pokemon.java
+++ src/main/java/com/poke/poke/pojo/Pokemon.java
@@ -2,12 +2,22 @@ package com.poke.poke.pojo;
 
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Pokemon {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id; 
 
     private String surname;
 
+    @OneToMany
     private List<Capacite> capacitesList;
 
 
